33de70e1567d2f6cf33136b83c68cf3da5ca443e
Falculista <falculista12@gmail.com>
2026-06-23 08:32:25 -0300

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
+
+// Interfaces (ver depois) Java Collections
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("a");
+        // JAVA COLLECTIONS:
+
+        // List => permite duplicatas [1,2,2,3,4,4]
+
+        List<String> garagem = new ArrayList<>();
+
+        // definimos uma ArrayList com List na esquerda para que, se no futuro precisarmos mudar...
+        // ...nós pudermos fazer isso facilmente, pois stringList é uma lista, mas não necessariamente...
+        // ...um ArrayList.
+
+        garagem.add("Gol");
+        garagem.add("BMW");
+        garagem.add("Audi");
+        garagem.add("Gol");
+
+        garagem.get(0);
+        garagem.get(1);
+        garagem.get(2);
+        garagem.get(3);
+
+        // Set => não permite duplicatas [1,2,3,4,5]
+
+
+
+        // Map => Estrutura de chave valor
+
+
+
+        // Queue => filas, usada normalmente para processamento de dados
+
 
     }
 }
